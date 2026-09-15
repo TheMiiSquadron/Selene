9cabe6029414642c3c7f4de4ef19f3b09d48a207
TheMiiSquadron <alexmarkham34@gmail.com>
2026-09-15 01:05:19 -0500

    Add native Apple client scaffold

---
+++ apple/Selene/Selene/SeleneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeleneApp.swift
+//  Selene
+//
+//  Created by Alex Markham on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct SeleneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apple/Selene/SeleneTests/SeleneTests.swift
@@ -0,0 +1,19 @@
+//
+//  SeleneTests.swift
+//  SeleneTests
+//
+//  Created by Alex Markham on 9/15/26.
+//
+
+import Testing
+@testable import Selene
+
+struct SeleneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
